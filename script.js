409a18b849748120efd1a119f45145d728d1b1f9
Joaquin Sosa <cto-joaquinsosa@hotmail.com>
2024-05-06 19:26:32 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let numerosDisponibles = [];
+
+window.onload = function () {
+    const bolillero = document.getElementById("bolillero");
+    for (let i = 1; i <= 99; i++) {
+        numerosDisponibles.push(i);
+        let card = document.createElement("div");
+        card.classList.add("card", "col-1", "m-1", "text-center");
+        card.innerText = i;
+        bolillero.appendChild(card);
+    }
+};
+
+function extraerNumero() {
+    if (numerosDisponibles.length === 0) {
+        alert("¡Todos los números han sido extraídos!");
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random() * numerosDisponibles.length);
+    let numeroExtraido = numerosDisponibles.splice(indiceAleatorio, 1)[0];
+
+    document.getElementById("numeroExtraido").innerText =
+        "Número Extraído: " + numeroExtraido;
+    $("#numeroExtraidoModal").modal("show");
+
+    cambiarFondoVerde(numeroExtraido);
+}
+
+function cambiarFondoVerde(numeroExtraido) {
+    let card = document.querySelector(
+        `#bolillero .card:nth-child(${numeroExtraido})`
+    );
+    card.classList.add("extraccion");
+}
